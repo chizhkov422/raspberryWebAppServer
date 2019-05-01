7e921bba46f78fc4aec06184a330f296f34b9ba7
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-05-01 14:22:32 +0300

    Change query

--- api/states.js
+++ api/states.js
@@ -18,24 +18,18 @@ module.exports = (mongooseModel, socketConnection) => {
 
     // socketConnection.emit('checkbox state', state.stateValue);
 
-    console.log('POST REQUEST UPDATE')
+    console.log('POST REQUEST UPDATE', state);
 
     if (state.stateName === 'temperature') {
       switch (state.mode) {
         case 'auto': {
-          mongooseModel.updateOne({ stateName: 'temperature' }, { mode: 'auto', minTemp: state.minTemp, maxTemp: state.maxTemp }, { upsert: true }).then(() => {
-            return res
-              .header("Access-Control-Allow-Origin", "*")
-              .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-              .send({ success: true, message: "Document updated!" });
+          mongooseModel.updateOne({ stateName: 'temperature' }, { $set: { mode: 'auto', minTemp: state.minTemp, maxTemp: state.maxTemp } }, { upsert: true }).then(() => {
+            return res.send({ success: true, message: "Document updated!" });
           });
         }
         case 'manual': {
-          mongooseModel.updateOne({ stateName: 'temperature' }, { mode: 'manual', manualTemp: state.manualTemp }, { upsert: true }).then(() => {
-            return res
-              .header("Access-Control-Allow-Origin", "*")
-              .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-              .send({ success: true, message: "Document updated!" });
+          mongooseModel.updateOne({ stateName: 'temperature' }, { $set: { mode: 'manual', manualTemp: state.manualTemp } }, { upsert: true }).then(() => {
+            return res.send({ success: true, message: "Document updated!" });
           });
         }
       }
